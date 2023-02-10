c0d1c7ba903fa2dbfb354299e97fc41bd268bf74
Stefan Hagen <stefan@hagen.link>
2023-02-10 20:48:42 +0100

    Added initial upstream implementation (mostly unchanged as seed)

---
+++ paikalta/api.py
@@ -0,0 +1,64 @@
+"""From the place (Finnish: paikalta) we derive the name - application programming interface."""
+import argparse
+import json
+import pathlib
+import re
+import sys
+
+from paikalta import (
+    APP_NAME,
+    APP_VERSION,
+    ENCODING,
+    FAIL,
+    INVALID_ID,
+    LOG_SEPARATOR,
+    QUIET,
+    SUCC,
+    TS_FORMAT_PAYLOADS,
+    VALID_NAME_PAT,
+    log,
+)
+
+
+def load(csaf_path):
+    """Load the CSAF data from the path to the JSON file or fail miserably."""
+    with open(csaf_path, 'rt', encoding=ENCODING) as handle:
+        return json.load(handle)
+
+
+def dump(csaf_data, csaf_path):
+    """Dump the CSAF data formated to 2 space indent to the JSON file given by the path."""
+    with open(csaf_path, 'wt', encoding=ENCODING) as f:
+        json.dump(csaf_data, f, indent=2)
+
+
+def compute_filename(csaf_data):
+    """Derive the filename from document/tracking/id if exists else return the conventional invalid json name."""
+    document_tracking_id = INVALID_ID
+    if (doc := csaf_data.get('document')) and (track := doc.get('tracking')) and (the_id := track.get('id')):  # noqa
+        document_tracking_id = the_id
+
+    return f'{re.sub(VALID_NAME_PAT, "_", document_tracking_id.lower())}.json'
+
+
+def process(options: argparse.Namespace):
+    """yes"""
+    data = load(options.input_file)
+    current_path = pathlib.Path(options.input_file)
+    correct_path = current_path.parent / compute_filename(data)
+    ok = current_path == correct_path
+
+    if options.echo:
+        print(correct_path.name)
+    if options.verbose:
+        print(SUCC if ok else FAIL)
+
+    if ok:
+        return 0
+
+    if options.add:
+        dump(data, correct_path)
+    elif options.update:
+        _ = current_path.rename(correct_path)
+
+    return 1
---
+++ paikalta/cli.py
@@ -0,0 +1,67 @@
+"""From the place (Finnish: paikalta) we derive the name - command line interface."""
+import argparse
+import json
+import pathlib
+import re
+import sys
+
+import paikalta.api as api
+from paikalta import (
+    APP_NAME,
+    APP_VERSION,
+    ENCODING,
+    FAIL,
+    INVALID_ID,
+    LOG_SEPARATOR,
+    QUIET,
+    SUCC,
+    TS_FORMAT_PAYLOADS,
+    VALID_NAME_PAT,
+    log,
+)
+
+
+def parser():
+    """Implementation of command line API returning parser."""
+    impl = argparse.ArgumentParser(description='Verifies or modifies the name of a CSAF 2.0 advisory file')
+    impl.add_argument('input_file', type=str, help='CSAF advisory file to verify or modify the filename of')
+    impl.add_argument('-p', '--print', dest='echo', action='store_true', help='Prints the correct filename')
+    impl.add_argument(
+        '-v',
+        '--verbose',
+        dest='verbose',
+        action='store_true',
+        help=f'Prints the logic result as either {SUCC} or {FAIL}',
+    )
+    impl.add_argument(
+        '-a',
+        '--add',
+        dest='add',
+        action='store_true',
+        help=(
+            'Writes the CSAF advisory file to the correct filename if different'
+            ' - will overrule -u/--update if given in addition'
+        ),
+    )
+    impl.add_argument(
+        '-u',
+        '--update',
+        dest='update',
+        action='store_true',
+        help=(
+            'Renames the CSAF advisory file to the correct filename if necessary'
+            ' - will be overruled by -a/--add if given in addition'
+        ),
+    )
+    return impl
+
+
+def app(argv=None):
+    """Drive the verification or modification of advisory file(name)s per CSAF 2.0 rules."""
+    argv = sys.argv[1:] if argv is None else argv
+    options = parser().parse_args(argv)
+    return api.process(options)
+
+
+if __name__ == '__main__':
+    sys.exit(app(sys.argv[1:]))
